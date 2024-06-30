8d7097c6528517b80412a964a8c34206824f93ec
ersinelmas <elmase23@itu.edu.tr>
2024-07-01 00:06:45 +0300

    interfaces and inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[3]
+            {
+                new Customer { Id = 1, Name = "Ali", Surname = "Veli", CreditCardNumber = "1234567890" },
+                new Student { Id = 2, Name = "Ayşe", Surname = "Fatma", StudentNumber = "9876543210" },
+                new Person { Id = 3, Name = "Hayri", Surname = "Kemal" }
+            };  
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.Name +" "+ person.Surname);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string CreditCardNumber { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string StudentNumber { get; set; }
+    }
+}
---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
+            };
+
+            foreach (var worker in workers) {
+                worker.Work();
+            }
+
+            IEater[] eaters = new IEater[2]
+            {
+                new Manager(),
+                new Worker()
+            };
+
+            foreach (var eater in eaters) {
+                eater.Eat();
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IWorker
+    {
+        void Work();
+    }
+
+    interface IEater
+    {
+        void Eat();
+    }
+
+    interface ISalary
+    {
+        void GetSalary();
+    }
+
+    class Manager : IWorker, IEater, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Manager is eating");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Manager is getting salary");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Manager is working");
+        }
+    }
+
+    class Worker : IWorker, IEater, ISalary
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Worker is eating");
+        }
+
+        public void GetSalary()
+        {
+            Console.WriteLine("Worker is getting salary");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine("Worker is working");
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Robot is working");
+        }
+    }
+}
